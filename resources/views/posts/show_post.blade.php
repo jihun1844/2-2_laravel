55d55b9404656174285e30142ba99e5c8ccc6769
jihun <tohka113@naver.com>
2023-10-31 21:32:27 +0900

    1031

--- resources/views/posts/show_post.blade.php
+++ resources/views/posts/show_post.blade.php
@@ -31,6 +31,39 @@
       @method("delete")
       <input type="submit" value="삭제"/>
     </form>
+
+    <hr>
+    <h4>댓글등록</h4>
+    <form action="/posts/{{$post->id}}/comments" method="post">
+      @csrf
+      <div>
+        <textarea name="content" id="" cols="30" rows="1"></textarea>
+      </div>
+      <input type="submit" value="등록">
+      <hr>
+
+
+      <div>
+        <h2>댓글 리스트({{$count}})</h2>
+          <table>
+            <tr>
+              <th> 연번</th><th>내용</th><th>작성자</th><th>작성일</th>
+            </tr>
+            @foreach($?? as $comment)
+            <tr>
+              <td>{{$loop->index+1}}</td>
+              <td>{{$comment->content}}</td>
+              <td>{{$comment->user_id}}</td>
+              <td>{{$comment->created_at}}</td>
+            </tr>
+            @endforeach
+          </table>
+      </div>
+
+
+
+    </form>
+
     <a href="/posts">돌아가기</a>
   </div>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
+use App\Models\Comment;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
@@ -24,3 +26,6 @@ Route::get('/', function () {
 Route::resource('/users', UserController::class);
 
 Route::resource('/posts', PostController::class);
+
+//중첩 메소드로 정의, nested resource                          except()는 라우트에 맵핑을 빼라는 뜻
+Route::resource('/posts.comments', CommentController::class)->except(['create']);
