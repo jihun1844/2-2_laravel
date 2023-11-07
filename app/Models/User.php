45f9bfc3e11116825aab06d5c57b6ed14e9bc913
jihun <tohka113@naver.com>
2023-11-07 12:38:55 +0900

    1107

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,28 @@ class User extends Authenticatable
     public function posts(){
         return $this->hasMany(Post::class);
     }
+
+    // public function latestPosts()
+    // {   //이 user가 작성한 posts 중에 가장 큰 놈
+    //     return $this->hasOne(Post::class)->latestOfMany();
+    // }
+
+    public function latestPosts()
+    {   //이 user가 작성한 posts 중에 가장 작은 놈
+        return $this->posts()->one()->ofMany('id', 'max');
+    }
+
+    public function oldestPosts()
+    {   //이 user가 작성한 posts 중에 가장 큰 놈
+        return $this->hasOne(Post::class)->oldestOfMany();
+    }
+
+    //내가 작성한 개시글의 모든 댓글 가져오기
+    //users --- posts --- comments
+    //   user_id, post_id, id, id
+    public function postcomments(){
+        //여러개의 코멘트를 포스트를 통해서 가진다
+        //return $this->hasManyThrough(Comment::class, Post::class);
+        return $this->hasManyThrough(Comment::class, Post::class, 'user_id', 'post_id', 'id', 'id');
+    }
 }
